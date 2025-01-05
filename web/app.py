7bca24a243b7caaf1790d302f1211abe60e374ce
David Catalano <david.catalano@gmail.com>
2025-01-05 19:21:53 +0000

    feat: hot reloading for gradio

--- web/app.py
+++ web/app.py
@@ -1,11 +1,17 @@
 import os
 
-import gradio as gr
+from watchfiles import run_process
 
-SERVER_PORT = int(os.getenv("SERVER_PORT", 5555))
 
-def greet(name: str) -> str:
-    return f"Hello, {name}!"
+def run_gradio():
+    import gradio as gr
+    SERVER_PORT = int(os.getenv("SERVER_PORT", 5555))
 
-with gr.Interface(fn=greet, inputs="text", outputs="text", title="Gradio Hello World") as demo:
-    demo.launch(server_port=SERVER_PORT, server_name="0.0.0.0")
+    def greet(name: str) -> str:
+        return f"Hello, {name}!"
+
+    with gr.Interface(fn=greet, inputs="text", outputs="text", title="Hello World") as demo:
+        demo.launch(server_port=SERVER_PORT, server_name="0.0.0.0")
+
+if __name__ == "__main__":
+    run_process(".", target=run_gradio)
