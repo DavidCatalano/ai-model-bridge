ee528c0afd55961a53f98031e5336124995d11ff
David Catalano <david.catalano@gmail.com>
2025-01-04 19:05:20 -0500

    chore: revised developer scripts
    - updated `devh bash` to use proper syntax for spawning a developer container
    - moved commands created for remote development (devr.sh) out of the developer host script (devh.sh)

--- scripts/devh.sh
+++ scripts/devh.sh
@@ -7,36 +7,14 @@ case "$1" in
     "build-no-cache")
         docker-compose build --no-cache
         ;;
-    "setup-pip")
-        docker exec -it modelbridge bash -c "
-            export PATH=\"\$PATH:\$HOME/.local/bin\" && \
-            python -m pip install --upgrade pip && \
-            pip install -r requirements-dev.txt && \
-            ruff check . --fix
-        " || {
-            echo 'Failed to set up the development environment.'
-            exit 1
-        }
-        ;;
-    "setup-bash")
-        docker exec -it modelbridge bash -c "
-            export PATH=\"\$PATH:\$HOME/.local/bin\" && \
-            if ! grep -Fxq 'source /opt/ai-model-bridge/miniconda/conda/bin/activate' ~/.bashrc; then
-                echo 'source /opt/ai-model-bridge/miniconda/conda/bin/activate' >> ~/.bashrc;
-            fi
-            if ! grep -Fxq 'conda activate /opt/ai-model-bridge/miniconda/env' ~/.bashrc; then
-                echo 'conda activate /opt/ai-model-bridge/miniconda/env' >> ~/.bashrc;
-            fi
-        " || {
-            echo 'Failed to set up the development environment.'
-            exit 1
-        }
-        ;;
     "bash")
-        docker exec -it modelbridge bash
+        docker-compose run --rm modelbridge --interactive $2
+        ;;
+    "attach")
+        docker exec -it modelbridge bash        
         ;;
     *)
-        echo "Usage: $0 {build-repo|build-no-cache|setup-pip|setup-bash|bash}"
+        echo "Usage: $0 {build-repo|setup-pip|setup-bash|bash}"
         exit 1
         ;;
 esac
--- scripts/devr.sh
+++ scripts/devr.sh
@@ -1,11 +1,38 @@
 #!/usr/bin/env bash
 
-# Check if the first argument is "bashrc"
-if [[ "$1" == "bashrc" ]]; then
-    echo "Appending Conda environment activation to ~/.bashrc"
-    echo "source /opt/ai-model-bridge/miniconda/conda/bin/activate && conda activate /opt/ai-model-bridge/miniconda/env" >> ~/.bashrc
-    echo "Done. The environment will activate automatically when you start a new shell."
-else
-    echo "Usage: devr.sh bashrc"
-    echo "Pass 'bashrc' as an argument to append Conda activation commands to ~/.bashrc."
-fi
+case "$1" in
+    "bashrc")
+        echo "Appending Conda environment activation to ~/.bashrc"
+        echo "source /opt/ai-model-bridge/miniconda/conda/bin/activate && conda activate /opt/ai-model-bridge/miniconda/env" >> ~/.bashrc
+        echo "Done. The environment will activate automatically when you start a new shell."
+        ;;
+    "setup-pip")
+        docker exec -it modelbridge bash -c "
+            export PATH=\"\$PATH:\$HOME/.local/bin\" && \
+            python -m pip install --upgrade pip && \
+            pip install -r requirements-dev.txt && \
+            ruff check . --fix
+        " || {
+            echo 'Failed to set up the development environment.'
+            exit 1
+        }
+        ;;
+    "setup-bash")
+        docker exec -it modelbridge bash -c "
+            export PATH=\"\$PATH:\$HOME/.local/bin\" && \
+            if ! grep -Fxq 'source /opt/ai-model-bridge/miniconda/conda/bin/activate' ~/.bashrc; then
+                echo 'source /opt/ai-model-bridge/miniconda/conda/bin/activate' >> ~/.bashrc;
+            fi
+            if ! grep -Fxq 'conda activate /opt/ai-model-bridge/miniconda/env' ~/.bashrc; then
+                echo 'conda activate /opt/ai-model-bridge/miniconda/env' >> ~/.bashrc;
+            fi
+        " || {
+            echo 'Failed to set up the development environment.'
+            exit 1
+        }
+        ;;
+    *)
+        echo "Usage: $0 {bashrc|setup-pip|setup-bash}"
+        exit 1
+        ;;
+esac
