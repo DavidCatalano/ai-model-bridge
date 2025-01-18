5509bff206c27e15371a32718500ca678cfdafeb
David Catalano <david.catalano@gmail.com>
2025-01-18 10:23:36 +0000

    fix: add test conversion script

---
+++ extensions/exllamav2_utils/test_convert.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+python exllamav2_utils_repo/convert.py \
+    -i /data/LLM/huggingface/hub/models--meta-llama--Llama-3.2-3B-Instruct/snapshots/0cb88a4f764b7a12671c53f0838cd831a0843b95/ \
+    -o /data/fast/modelbridge/test02/ \
+    -cf /data/fast/modelbridge/test02/ \
+    -b 4.5
